bc9675bb4ec5afb13bfe13e02cd01474b2b9cf1f
ampck <a1j2m4@gmail.com>
2023-08-21 15:48:54 -0400

    3. Testing Contracts

--- test/Token.js
+++ test/Token.js
@@ -4,9 +4,9 @@ const tokens = (n) => {
 	return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 describe('Token', () => {
-	let token
-	let accounts
-	let deployer
+	let token,
+		accounts,
+		deployer
 	beforeEach(async () => {
 		const Token = await ethers.getContractFactory('Token')
 		token = await Token.deploy('Dapp University', 'DAPP', 1000000)
