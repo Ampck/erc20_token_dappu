9544f771bf0b8f924b7a294f89c3846074af0b37
ampck <a1j2m4@gmail.com>
2023-08-22 12:42:32 -0400

    6. Token approvals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,10 +10,16 @@ contract Token {
     uint256 public decimals = 18;
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(
         address indexed from,
         address indexed to,
+        uint256 value
+    );
+    event Approval(
+        address indexed owner,
+        address indexed spender,
         uint256 value
     );
     
@@ -37,6 +43,20 @@ contract Token {
 
         balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
         balanceOf[_to] = balanceOf[_to] + _value;
+        
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(_spender != address(0));
+
+        allowance[msg.sender][_spender] = _value;
+        
+        emit Approval(msg.sender, _spender, _value);
+        return true;
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -14,8 +14,10 @@ describe('Token', () => {
 		token = await Token.deploy('Dapp University', 'DAPP', 1000000)
 
 		accounts = await ethers.getSigners()
+		emptyAddress = '0x0000000000000000000000000000000000000000'
 		deployer = accounts[0]
 		receiver = accounts[1]
+		exchange = accounts[2]
 	})
 	
 	describe('Deployment', () => {
@@ -62,17 +64,43 @@ describe('Token', () => {
 				expect(event.args['value']).to.equal(amount)
 			})
 	 	})
-	 	describe("Failure", () => {
-	 		beforeEach(async () =>{
-
-			})
+	 	describe('Failure', () => {
 			it('rejects insufficient balances', async () => {
 				const invalidAmount = tokens(100000000)
 				await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted
 			})
 			it('rejects invalid recipent', async () => {
-				await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+				await expect(token.connect(deployer).transfer(emptyAddress, amount)).to.be.reverted
 			})
 	 	})
+	})
+
+	describe('Approving Tokens', () => {
+		let amount,
+		 	transaction,
+		 	result,
+		 	event
+		beforeEach(async () => {
+			amount = tokens(100)
+			transaction = await token.connect(deployer).approve(exchange.address, amount)
+			result = await transaction.wait()
+		})
+		describe('Success', () => {
+			it('allocates an allowance for delegated token spending', async () => {
+				expect(await token.allowance(deployer.address, exchange.address)).to.equal(amount)
+			})
+			it('emits and approval event', async () => {
+				event = result.events[0]
+				expect(event.event).to.equal("Approval")
+				expect(event.args['owner']).to.equal(deployer.address)
+				expect(event.args['spender']).to.equal(exchange.address)
+				expect(event.args['value']).to.equal(amount)
+			})
+		})
+		describe('Failure', () => {
+			it('rejects invalid spender', async () => {
+				await expect(token.connect(deployer).approve(emptyAddress, amount)).to.be.reverted
+			})
+		})
 	})
 })
