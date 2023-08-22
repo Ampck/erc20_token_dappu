49c87af0f8b0b5523e7bd2c9f65804f824ed64c2
ampck <a1j2m4@gmail.com>
2023-08-22 12:49:32 -0400

    6. Token approvals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -34,16 +34,27 @@ contract Token {
         balanceOf[msg.sender] = totalSupply;
     }
 
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _value
+    ) internal {
+        require(_to != address(0));
+
+        balanceOf[_from] = balanceOf[_from] - _value;
+        balanceOf[_to] = balanceOf[_to] + _value;
+        
+        emit Transfer(_from, _to, _value);
+    }
+
     function transfer(address _to, uint256 _value)
         public
         returns (bool success)
     {
         require(balanceOf[msg.sender] >= _value);
-        require(_to != address(0));
 
-        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
-        balanceOf[_to] = balanceOf[_to] + _value;
-        
+        _transfer(msg.sender, _to, _value)
+
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
@@ -58,5 +69,16 @@ contract Token {
         
         emit Approval(msg.sender, _spender, _value);
         return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) 
+        public
+        returns (bool success);
+    {
+
     }
 }
